4edf2a12c99b4eb322effd7a7af9c38f7e55e2e1
Marek Ramotowski <ma-ra@users.noreply.github.com>
2018-11-05 10:44:38 +0100

    Stronger scaling due to longer company names.

--- gspdf-slicer.bat
+++ gspdf-slicer.bat
@@ -33,7 +33,7 @@ REM 1
 %gswin% ^
     -o "run1\1.pdf" ^
     -sDEVICE=pdfwrite ^
-    -c "[/CropBox [0 0 515 140]" ^
+    -c "[/CropBox [0 0 560 140]" ^
     -c " /PAGES pdfmark" ^
 	-c "<</BeginPage{1 1 scale -10 -713 translate}>> setpagedevice" ^
     -f run0\source.pdf
@@ -42,7 +42,7 @@ REM 2
 %gswin% ^
     -o "run1\2.pdf" ^
     -sDEVICE=pdfwrite ^
-    -c "[/CropBox [0 0 515 140]" ^
+    -c "[/CropBox [0 0 560 140]" ^
     -c " /PAGES pdfmark" ^
 	-c "<</BeginPage{1 1 scale -10 -572 translate}>> setpagedevice" ^
     -f run0\source.pdf
@@ -51,7 +51,7 @@ REM 3
 %gswin% ^
     -o "run1\3.pdf" ^
     -sDEVICE=pdfwrite ^
-    -c "[/CropBox [0 0 515 140]" ^
+    -c "[/CropBox [0 0 560 140]" ^
     -c " /PAGES pdfmark" ^
 	-c "<</BeginPage{1 1 scale -10 -432 translate}>> setpagedevice" ^
     -f run0\source.pdf
@@ -60,7 +60,7 @@ REM 4
 %gswin% ^
     -o "run1\4.pdf" ^
     -sDEVICE=pdfwrite ^
-    -c "[/CropBox [0 0 515 140]" ^
+    -c "[/CropBox [0 0 560 140]" ^
     -c " /PAGES pdfmark" ^
 	-c "<</BeginPage{1 1 scale -10 -292 translate}>> setpagedevice" ^
     -f run0\source.pdf
@@ -69,7 +69,7 @@ REM 5
 %gswin% ^
     -o "run1\5.pdf" ^
     -sDEVICE=pdfwrite ^
-    -c "[/CropBox [0 0 515 140]" ^
+    -c "[/CropBox [0 0 560 140]" ^
     -c " /PAGES pdfmark" ^
 	-c "<</BeginPage{1 1 scale -10 -153 translate}>> setpagedevice" ^
     -f run0\source.pdf
@@ -78,7 +78,7 @@ REM 6
 %gswin% ^
     -o "run1\6.pdf" ^
     -sDEVICE=pdfwrite ^
-    -c "[/CropBox [0 0 515 140]" ^
+    -c "[/CropBox [0 0 560 140]" ^
     -c " /PAGES pdfmark" ^
 	-c "<</BeginPage{1 1 scale -10 -15 translate}>> setpagedevice" ^
     -f run0\source.pdf
@@ -153,7 +153,7 @@ REM ###############
 if not exist run4 mkdir run4
 del run4\*.pdf
 
-gsar -s"/CropBox [0 0 515.0 140.0]" -r -f run3\merge.pdf run4\gsar.pdf
+gsar -s"/CropBox [0 0 560.0 140.0]" -r -f run3\merge.pdf run4\gsar.pdf
 	
 %gswin% ^
     -o "run0\output.pdf" ^
@@ -161,7 +161,7 @@ gsar -s"/CropBox [0 0 515.0 140.0]" -r -f run3\merge.pdf run4\gsar.pdf
 	-dDEVICEWIDTHPOINTS=425 ^
 	-dDEVICEHEIGHTPOINTS=283 ^
 	-dFIXEDMEDIA ^
-	-c "<</BeginPage{0.82 1 scale 0 60 translate}>> setpagedevice" ^
+	-c "<</BeginPage{0.75 1 scale 0 60 translate}>> setpagedevice" ^
     -f "run4\gsar.pdf"
 
 copy /V /Y "run0\output.pdf" output\%output%	
